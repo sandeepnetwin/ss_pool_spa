292cc4edec8dc5acb62990fa4c423c2b998be5df
sandeepnetwin <sandeep.w@netwinindia.biz>
2015-07-07 18:17:55 +0530

    Comment invalid code
    Comment invalid code

--- include/config.php
+++ include/config.php
@@ -23,12 +23,12 @@ $database = "ss_poolspa_db";
 $db_username = "root";
 $db_password = "";
 
-*/
+
 define('DB_HOST', $host);
 define('DB_DATABASE', $database);
 define('DB_USERNAME', $db_username);
 define('DB_PASSWORD', $db_password);
-
+*/
 mysql_connect(DB_HOST, DB_USERNAME, DB_PASSWORD) or die("Error: Could not connect to the server.");
 mysql_select_db(DB_DATABASE) or die("Error: Could not select database");
 
